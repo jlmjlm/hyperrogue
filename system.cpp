99645bfe13e5580fa9c683b9bd6e833e1d1e00d5
Jacob Mandelson <jlm@ofb.net>
2024-04-20 16:20:18 -0700

    Use OoAether for lands which can wall the player in.

--- system.cpp
+++ system.cpp
@@ -260,19 +260,15 @@ EX void initgame() {
       }
     }
   
-  if(/* tactic::on && */ firstland == laPower) {
+  if (/* tactic::on && */ firstland == laPower) {
     items[itOrbSpeed] = 30;
     items[itOrbWinter] = 30;
     items[itOrbFlash] = 30;
     }
-  if(firstland == laDeadCaves || firstland == laClearing)
+  if (firstland == laClearing)
     items[itOrbFlash] = max(items[itOrbFlash], 5);
-  if(firstland == laWet || firstland == laCursed) {
-    items[itOrbFlash] = max(items[itOrbFlash], 5);
-    items[itOrbFish] = max(items[itOrbFish], 5);
-    }
-  if(firstland == laVolcano)
-    items[itOrbWinter] = max(items[itOrbWinter], 5);
+  if (among(firstland, laCaves, laDeadCaves, laWet, laCursed, laVolcano))
+    items[itOrbAether] = max(items[itOrbAether], 5);
   
   if(firstland == laCA)
     items[itOrbAether] = 2;
